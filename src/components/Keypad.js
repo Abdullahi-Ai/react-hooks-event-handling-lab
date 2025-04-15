ed3565195af79ce07d58ef5741addebecb8370de
Abdullahi-Ai <abdullahiibrashair@gmail.com>
2025-04-15 12:00:55 +0300

    react-hooks-event-handling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
-    return (
-        <div></div>
+    
+        function changeFn(){
+            console.log("Entering password...");
+        }
+        return(
+            <div>
+                <input type="password" onChange={changeFn} />
+                </div>
     )
 }
 
